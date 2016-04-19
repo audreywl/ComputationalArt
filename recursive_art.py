71c79650a8ebaad69f685f5c1106b54cb1aab231
audreywl <audrey.lewis@students.olin.edu>
2016-04-18 20:17:10 -0400

    done with all the going beyonds for MP5

--- recursive_art.py
+++ recursive_art.py
@@ -2,7 +2,11 @@
 
 import random, math
 from PIL import Image
-
+import pygame
+from pygame.locals import QUIT
+import alsaaudio
+import audioop
+import time
 
 def build_random_function(min_depth, max_depth):
 	""" Builds a random function of depth at least min_depth and depth
@@ -18,10 +22,10 @@ def build_random_function(min_depth, max_depth):
 
 
 	if max_depth < 1:
-		return random.choice([lambda x,y: x, lambda x,y: y])
+		return random.choice([lambda x,y,t: x, lambda x,y,t: y, lambda x,y,t: t])
 	elif min_depth < 1:
 		if random.randint(0,1):
-			return random.choice([lambda x,y: x, lambda x,y: y])
+			return random.choice([lambda x,y,t: x, lambda x,y,t: y, lambda x,y,t: t])
 
 	new_min_depth= min_depth-1
 	new_max_depth = max_depth-1
@@ -29,12 +33,12 @@ def build_random_function(min_depth, max_depth):
 	next_function_1 = build_random_function(new_min_depth, new_max_depth)
 	next_function_2 = build_random_function(new_min_depth, new_max_depth)
 
-	functions = [lambda x,y: next_function_1(x,y)*next_function_2(x,y), 
-	lambda x,y: .5*next_function_1(x,y)*next_function_2(x,y), 
-	lambda x,y: math.cos(2* math.pi * next_function_1(x,y)), 
-	lambda x,y: math.sin(2* math.pi * next_function_1(x,y)),
-	lambda x,y: next_function_1(x,y)*next_function_1(x,y), 
-	lambda x,y: math.sqrt(abs(next_function_1(x,y)))]
+	functions = [lambda x,y,t: next_function_1(x,y,t)*next_function_2(x,y,t), #product
+	lambda x,y,t: .5*next_function_1(x,y,t)*next_function_2(x,y,t), #average
+	lambda x,y,t: math.cos(2* math.pi * next_function_1(x,y,t)), #sine pi
+	lambda x,y,t: math.sin(2* math.pi * next_function_1(x,y,t)), #cosine pi
+	lambda x,y,t: next_function_1(x,y,t)*next_function_1(x,y,t), #square
+	lambda x,y,t: math.sqrt(abs(next_function_1(x,y,t)))] #square root
 
 	
 	return random.choice(functions)
@@ -161,6 +165,34 @@ def test_image(filename, x_size=350, y_size=350):
 	im.save(filename)
 
 
+def generate_movie(filename, x_size=350, y_size=350, movie_frames=50):
+	""" Generate computational art and save as an image file.
+
+		filename: string filename for image (should be .png)
+		x_size, y_size: optional args to set image dimensions (default: 350)
+	"""
+	# Functions for red, green, and blue channels - where the magic happens!
+	red_function = build_random_function(6,9)
+	green_function = build_random_function(6,9)
+	blue_function = build_random_function(6,9)
+	# Create image and loop over all pixels
+	im = Image.new("RGB", (x_size, y_size))
+	pixels = im.load()
+	for time in range(movie_frames):
+		frame_name = filename + str(time) + '.png'
+		t = remap_interval(time, 0, movie_frames, -1, 1)
+		for i in range(x_size):
+			for j in range(y_size):
+				x = remap_interval(i, 0, x_size, -1, 1)
+				y = remap_interval(j, 0, y_size, -1, 1)
+				pixels[i, j] = (
+						color_map(red_function(x,y,t)),
+						color_map(green_function(x,y,t)),
+						color_map(blue_function(x,y,t))
+						)
+
+		im.save(frame_name)
+
 def generate_art(filename, x_size=350, y_size=350):
 	""" Generate computational art and save as an image file.
 
@@ -174,30 +206,57 @@ def generate_art(filename, x_size=350, y_size=350):
 	# Create image and loop over all pixels
 	im = Image.new("RGB", (x_size, y_size))
 	pixels = im.load()
+	t=1
 	for i in range(x_size):
 		for j in range(y_size):
 			x = remap_interval(i, 0, x_size, -1, 1)
 			y = remap_interval(j, 0, y_size, -1, 1)
 			pixels[i, j] = (
-					color_map(red_function(x,y)),
-					color_map(green_function(x,y)),
-					color_map(blue_function(x,y))
+					color_map(red_function(x,y,t)),
+					color_map(green_function(x,y,t)),
+					color_map(blue_function(x,y,t))
 					)
-
 	im.save(filename)
 
-
+def music_visualizer(file_extension, volume_precision, x_size=350, y_size=350):
+	"""creates music visualizer using pygame, pyalsa, and functions from recursive art."""
+	#audio setup
+	inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,0)
+	inp.setchannels(1)
+	inp.setrate(16000)
+	inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+	inp.setperiodsize(160)
+	#generate frames
+	#generate_movie(file_extension, x_size, y_size, volume_precision)
+	#pygame load list of screens to choose from
+	surface_list = []
+	for i in range(volume_precision):
+		filename=file_extension+str(i)+'.png'
+		current_image = pygame.image.load(filename)
+		#current_image.convert()
+		surface_list.append(current_image)
+	#start visualizer
+	pygame.init()
+	size=(x_size, y_size)
+	screen= pygame.display.set_mode(size)
+	running= True
+	while running:
+		for event in pygame.event.get():
+			if event.type== QUIT:
+				running= False
+		l,data = inp.read()
+		if l:
+			   loudness = audioop.rms(data,2)
+		
+		frame = int(remap_interval(loudness, 500, 5000, 1, volume_precision))
+		try:
+			current_surface = surface_list[frame]
+		except IndexError:
+			current_surface = surface_list[49]
+		screen.blit(current_surface, (0,0))
+		pygame.display.update()
+		time.sleep(.001)
 if __name__ == '__main__':
 	import doctest
-	# doctest.testmod()
-
-	# Create some computational art!
-	# TODO: Un-comment the generate_art function call after you
-	#       implement remap_interval and evaluate_random_function
-generate_art("myart15.png")
-#remap_interval(5, 4, 6, 1, 2)
-#print build_random_function(1,3)
-
-	# Test that PIL is installed correctly
-	# TODO: Comment or remove this function call after testing PIL install
-	#test_image("noise.png")
+	#generate_art("myart17.png")
+	music_visualizer('frame', 50)
